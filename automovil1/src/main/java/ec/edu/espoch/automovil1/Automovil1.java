441097736d2fc4c1b0656c2b0e7bc39825e2cf2c
JIrleidySantos <jirleidysantosm@gmail.com>
2024-11-29 16:51:38 -0500

    Creacion

--- automovil1/src/main/java/ec/edu/espoch/automovil1/Automovil1.java
+++ automovil1/src/main/java/ec/edu/espoch/automovil1/Automovil1.java
@@ -11,6 +11,6 @@ package ec.edu.espoch.automovil1;
 public class Automovil1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World1!");
     }
 }
