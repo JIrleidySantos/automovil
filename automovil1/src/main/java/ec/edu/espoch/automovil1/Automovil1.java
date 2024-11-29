d2be82d69b80d69c82a4ec525eb5f332d9f8cd07
JIrleidySantos <jirleidysantosm@gmail.com>
2024-11-29 16:46:53 -0500

    Creacion

---
+++ automovil1/src/main/java/ec/edu/espoch/automovil1/Automovil1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.automovil1;
+
+/**
+ *
+ * @author mundo
+ */
+public class Automovil1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
